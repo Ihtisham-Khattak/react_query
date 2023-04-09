748e326b88242c6b975033823a1bc351a04dcfe2
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-04-09 15:40:04 +0500

    pagination

--- src/components/planet/Planet.js
+++ src/components/planet/Planet.js
@@ -3,8 +3,8 @@ import { useQuery } from "react-query";
 import PlanetComponent from "./PlanetComponent";
 import Loader from "react-js-loader";
 
-const fetchPlanet = async (key, greeting, page) => {
-  const res = await fetch(`https://swapi.dev/api/planets/?page=${2}`);
+const fetchPlanet = async (key, page) => {
+  const res = await fetch('https://swapi.dev/api/planets/?page=2');
   return res.json();
 };
 const Planet = () => {
@@ -12,16 +12,17 @@ const Planet = () => {
   // 2. {} contains data (fetch data from the API), status(loading, error, pending)
   // 3. Query variable
 
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
+  console.log("🚀 ~ file: Planet.js:16 ~ Planet ~ page:", page)
 
-  const { data, status } = useQuery(["Planet", "page", page], fetchPlanet);
+  const { data, isFetching, isPreviousData, status } = useQuery(
+    ["Planet", page],
+    fetchPlanet
+  );
   console.log("🚀 ~ file: Planet.js:17 ~ Planet ~ data:", data);
 
   return (
     <div>
-      {/* <button onClick={() => setPage(1)}>1</button>
-      <button onClick={() => setPage(2)}>2</button>
-      <button onClick={() => setPage(3)}>3</button> */}
       <h2>
         {(status === "error" && (
           <div>
@@ -40,11 +41,34 @@ const Planet = () => {
             </div>
           )) ||
           (status === "success" && (
-            <div>
-              {data.results.map((planet, index) => (
-                <PlanetComponent planet={planet} index={index} />
-              ))}
-            </div>
+            <>
+              <h4>Current Page: {page}</h4>
+              <div>
+                {data.results.map((planet, index) => (
+                  <PlanetComponent planet={planet} index={index} />
+                ))}
+              </div>
+              <button
+                className="button"
+                onClick={() => setPage((old) => Math.max(old - 1, 0))}
+                disabled={page === 0}
+              >
+                Prev Page
+              </button>{" "}
+              <button
+                className="button"
+                onClick={() => {
+                  if (!isPreviousData && data.hasMore) {
+                    setPage((old) => old + 1);
+                  }
+                }}
+                // Disable the Next Page button until we know a next page is available
+                disabled={isPreviousData || !data?.hasMore}
+              >
+                Next Page
+              </button>
+              {isFetching ? <span> Loading...</span> : null}{" "}
+            </>
           ))}
       </h2>
     </div>
